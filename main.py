2aa5eb0a6aeabd8e1df4feaa2c2d817fe782f515
sharad1126 <sharad1126@rediff.com>
2016-09-04 22:27:41 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+def create_project_dir(directory):
+	if not os.path.exists(directory):
+		print 'creating project ' + directory
+		os.makedirs(directory)
+
+def create_data_files(project_name, base_url):
+	queue = project_name + '/queue.txt'
+	crawled = project_name + '/crawled.txt'
+	if not os.path.isfile(queue):
+		write_file(queue, base_url)
+	if not os.path.isfile(crawled):
+		write_file(crawled, '')
+	 	
+def write_file(path, data):
+	f = open(path, 'w')
+	f.write(data)
+	f.close()
+
+create_data_files('test', 'http://collegexplorer.co.in/')
+
+
